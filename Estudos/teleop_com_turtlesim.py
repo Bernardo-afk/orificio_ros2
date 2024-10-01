49fec83def4c55b8d7dcf931ce5b62b03ef5c1fa
snow <bernardoatadia0609@gmail.com>
2024-10-01 10:31:15 -0300

    .

--- Estudos/teleop_com_turtlesim.py
+++ Estudos/teleop_com_turtlesim.py
@@ -17,3 +17,11 @@ def generate_launch_description():
         )
     ]
     )
+
+
+
+    ros2 topic pub --once turtle1/cmd_vel geometry_msgs/msg/Twist "{linear: {x: 4.0, y : 3.0, z: 10.0}, angular: {x: 0.0, y: 3.0, z: 0.0}}"
+  
+ros2 topic pub --once turtle1/cmd_vel geometry_msgs/msg/Twist "{linear: {x: -4.0, y : 0.0, z: 0.0}, angular: {x: 1.0, y: 2.0, z: 6.0}}"
+
+ros2 topic pub --once turtle1/cmd_vel geometry_msgs/msg/Twist "{linear: {x: 4.0, y : 0.0, z: 0.0}, angular: {x: 1.0, y: 2.0, z: 6.0}}"
